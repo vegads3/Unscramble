3457677471935b4f3e1a5705f41621918e399c90
vegads3 <tristannbautista02@gmail.com>
2025-11-11 17:18:51 -0600

    recien actualizado

---
+++ app/src/main/java/com/example/unscramble/data/WordsData.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.unscramble.data
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+// Set with all the words for the Game
+val allWords: Set<String> =
+    setOf(
+        "animal",
+        "carro",
+        "anecdota",
+        "abecedario",
+        "todo",
+        "asombroso",
+        "alrededor",
+        "baloncesto",
+        "barra",
+        "basket",
+        "bebé",
+        "camara",
+        "campamento",
+        "dedo",
+        "delimitar",
+        "encender",
+        "elefante",
+        "esmeralda",
+        "final",
+        "flores",
+        "gastar",
+        "guitarra",
+        "grocería",
+        "ice",
+        "ingaturroña",
+        "joker",
+        "jochis",
+        "jornada",
+        "pintar",
+        "persona",
+        "picnic",
+        "almohada",
+        "pizza",
+        "podcast",
+        "presentacion",
+        "puppy",
+        "puzzle",
+        "recipiente",
+        "corredor",
+        "restaurant",
+        "turista",
+        "viajes",
+        "camioneta",
+        "abajo",
+        "usable",
+        "unicornio",
+        "new year",
+        "yummy",
+        "zebra",
+        "zigzag",
+        "zoologico",
+        "zona",
+        "zambrano"
+    )
